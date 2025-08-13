3af2d7acffad198245e667929e6ebf401e747a91
Labhesh Patil <labheshp2003@gmail.com>
2025-08-14 02:13:48 +0530

    Started Encoding
    Functions including do_encoding, open_file, check_capcity, copy_bmp_header implemented

--- encode.c
+++ encode.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "encode.h"
 #include "types.h"
+#include "string.h"
 
 /* Function Definitions */
 
@@ -71,5 +72,95 @@ Status open_files(EncodeInfo *encInfo)
     }
 
     // No failure return e_success
+    return e_success;
+}
+
+Status read_and_validate_encode_args(char **argv,EncodeInfo *encInfo){
+    if(strstr(argv[2],".bmp") != NULL){
+        encInfo->src_image_fname = argv[2];
+    }
+    else{
+        return e_failure;
+    }
+
+    if(strstr(argv[3],".txt") != NULL){
+        encInfo->secret_fname = argv[3];
+    }
+    else{
+        return e_failure;
+    }
+
+    if (argv[4] != NULL)
+    {
+        if (strstr(argv[4], ".bmp") != NULL)
+        {
+            encInfo->stego_image_fname = argv[4];
+        }
+        else
+        {
+            return e_failure;
+        }
+    }
+    else{
+        encInfo->stego_image_fname = "stego.bmp";
+    }
+
+    return e_success;
+}
+
+uint get_file_size(FILE *fptr){
+    fseek(fptr, 0, SEEK_END);
+    return ftell(fptr);
+}
+
+Status check_capacity(EncodeInfo *encInfo){
+    //sizeof beautiful.bmp > (16+32+ 32+ 32+ (sizeof secret.txt *8 ))
+    encInfo->image_capacity = get_image_size_for_bmp(encInfo->fptr_src_image);
+    encInfo->size_secret_file=get_file_size(encInfo->fptr_secret);
+    if(encInfo->image_capacity > (16 + 32 + 32 + 32 + (encInfo->size_secret_file *8))){
+        return e_success;
+    }
+    else{
+        return e_failure;
+    }
+}
+
+Status copy_bmp_header(FILE *fptr_src_image, FILE *fptr_dest_image){
+    char str[54];
+    fseek(fptr_src_image,0,SEEK_SET);
+    int byteRead = fread(str,1,54,fptr_src_image);
+    if(byteRead != 54){
+        printf("Error: only read %d bytes\n", byteRead);
+        return e_failure;
+    }
+    fseek(fptr_dest_image,0,SEEK_SET);
+    fwrite(str,1,54,fptr_dest_image);
+    return e_success;
+}
+
+Status do_encoding(EncodeInfo *encInfo){
+    if(open_files(encInfo) == e_success){
+        printf("Open file is a success\n");
+    }
+    else{
+        printf("Error opening file\n");
+        return e_failure;
+    }
+
+    if(check_capacity(encInfo) == e_success){
+        printf("%s file can be used to perform encoding\n",encInfo->src_image_fname);
+    }
+    else{
+        printf("%s file cannot be used to perform encoding\n",encInfo->src_image_fname);
+        return e_failure;
+    }
+
+    if(copy_bmp_header(encInfo->fptr_src_image, encInfo->fptr_stego_image) == e_success){
+        printf("Copied bmp header of %s to %s\n", encInfo -> src_image_fname, encInfo -> stego_image_fname);
+    }
+    else{
+        printf("Failed to copy bmp header of %s to %s\n", encInfo -> src_image_fname, encInfo -> stego_image_fname);
+        return e_failure;
+    }
     return e_success;
 }
--- test_encode.c
+++ test_encode.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "encode.h"
 #include "types.h"
+#include <string.h>
 
-int main()
+/* int main()
 {
     EncodeInfo encInfo;
     uint img_size;
@@ -28,4 +29,47 @@ int main()
     printf("INFO: Image size = %u\n", img_size);
 
     return 0;
+}  */
+
+int main(int argc,char **argv){
+    if(check_operation_type(argv) == e_encode){
+        printf("User selected encoding.\n");
+        EncodeInfo encoding;
+        if(read_and_validate_encode_args(argv,&encoding) == e_success){
+            printf("Read and validate encode arguments is a success\n");
+            printf("Started encoding..........\n");
+            if(do_encoding(&encoding) == e_success){
+                printf("Encoding completed successfully\n");
+            }
+            else{
+                printf("Failed to perform encoding\n");
+                return -1;
+            }
+        }
+        else{
+            printf("Read and validate encode arguments is a failure\n");
+            return 1;
+        }
+    }
+    else if (check_operation_type(argv) == e_decode){
+        printf("User selected decoding.\n");
+    }
+    else{
+        printf("Invalid Input\n");
+        printf("For encoding : ./a.out -e beautiful.bmp secret.txt [stego.bmp]\n");
+        printf("For decoding : ./a.out -d stego.bmp [decode.txt]\n");
+    }
+    return 0;
 }
+
+OperationType check_operation_type(char **argv){
+    if(strcmp(argv[1],"-e") == 0){
+         return e_encode;
+    }
+    else if(strcmp(argv[1],"-d") == 0){
+        return e_decode;
+    }
+    else{
+        return e_unsupported;
+    }
+}   
